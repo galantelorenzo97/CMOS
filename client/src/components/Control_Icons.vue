2389565478b45f3d631561e461d90397456f5c09
Lorenzo Galante <galantelorenzo97@gmail.com>
2020-03-12 04:26:24 -0400

    Delete Button Functionality Added; Added unused control_icons

--- client/src/components/Control_Icons.vue
+++ client/src/components/Control_Icons.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="container">
-        <button class="button is-info is-small">
+        <button class="button is-info is-small" @click="infoButtonPressed=true">
             <span class="icon">
                 <i class="fas fa-bars" aria-hidden="true"></i>
             </span>
         </button>
-        <button class="button is-warning is-small">
+        <button class="button is-warning is-small" @click="moveButtonPressed=true">
             <span class="icon">
                 <i class="fas fa-truck-loading" aria-hidden="true"></i>
             </span>
         </button>
-        <button class="button is-danger is-small">
+        <button class="button is-danger is-small" @click="deleteButtonPressed=true">
             <span class="icon">
                 <i class="fas fa-trash" aria-hidden="true"></i>
             </span>
@@ -19,8 +19,16 @@
 </template>
 
 <script>
-export default {
+export let deleteButtonPressed = false;
+export let infoButtonPressed = false;
+export let moveButtonPressed = false;
 
+export default {
+    data: () => ({
+      deleteButtonPressed,
+      infoButtonPressed,
+      moveButtonPressed  
+    })
 }
 </script>
 
--- client/src/views/Computers.vue
+++ client/src/views/Computers.vue
@@ -1,57 +1,105 @@
 <template>
   <section class="container">
     <div class="columns has-text-centered">
-        <div class="column">
-            <h1 class="title has-background-primary">Active Computers</h1>
-            <table class="table is-striped is-hoverable">
-                <thead>
-                    <th> Service Tag </th>
-                    <th> User </th>
-                    <th> Controls </th>
-                </thead>
-                <tbody>
-                    <tr v-for="(computerlist) in active_CI" :key=computerlist.Service_Tag>
-                        <th> {{computerlist.Service_Tag}} </th>
-                        <td> {{computerlist.Assigned_User}}</td>
-                        <td> <controlIcons></controlIcons> </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="column">
-            <h1 class="title has-background-warning">Stored Computers</h1>
-            <table class="table is-striped is-hoverable">
-                <thead>
-                    <th> Service Tag </th>
-                    <th> Location </th>
-                    <th> Controls </th>
-                </thead>
-                <tbody>
-                    <tr v-for="(storedmachine) in stored_CI" :key=storedmachine.Service_Tag>
-                        <th> {{storedmachine.Service_Tag}} </th>
-                        <td> {{storedmachine.Location}}</td>
-                        <td> <controlIcons></controlIcons> </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="column">
-            <h1 class="title has-background-danger">Retired Computers</h1>
-            <table class="table is-striped is-hoverable">
-                <thead>
-                    <th> Service Tag </th>
-                    <th> Location </th>
-                    <th> Controls </th>
-                </thead>
-                <tbody>
-                    <tr v-for="(retiredmachine) in retired_CI"  :key=retiredmachine.Service_Tag >
-                        <th> {{retiredmachine.Service_Tag}} </th>
-                        <td> {{retiredmachine.Location}} </td>
-                        <td> <controlIcons></controlIcons> </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+      <div class="column">
+        <h1 class="title has-background-primary">Active Computers</h1>
+        <table class="table is-striped is-hoverable">
+          <thead>
+            <th>Service Tag</th>
+            <th>User</th>
+            <th>Controls</th>
+          </thead>
+          <tbody>
+            <tr v-for="(computerlist, i) in active_CI" :key="computerlist.Service_Tag">
+              <th>{{computerlist.Service_Tag}}</th>
+              <td>{{computerlist.Assigned_User}}</td>
+              <td>
+                <button class="button is-info is-small" @click="viewInfo(i)">
+                  <span class="icon">
+                    <i class="fas fa-bars" aria-hidden="true"></i>
+                  </span>
+                </button>
+                <button class="button is-warning is-small">
+                  <span class="icon">
+                    <i class="fas fa-truck-loading" aria-hidden="true"></i>
+                  </span>
+                </button>
+                <button class="button is-danger is-small" @click="deleteComputer(active_CI,i)">
+                  <span class="icon">
+                    <i class="fas fa-trash" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="column">
+        <h1 class="title has-background-warning">Stored Computers</h1>
+        <table class="table is-striped is-hoverable">
+          <thead>
+            <th>Service Tag</th>
+            <th>Location</th>
+            <th>Controls</th>
+          </thead>
+          <tbody>
+            <tr v-for="(storedmachine, i) in stored_CI" :key="storedmachine.Service_Tag">
+              <th>{{storedmachine.Service_Tag}}</th>
+              <td>{{storedmachine.Location}}</td>
+              <td>
+                <button class="button is-info is-small">
+                  <span class="icon">
+                    <i class="fas fa-bars" aria-hidden="true"></i>
+                  </span>
+                </button>
+                <button class="button is-warning is-small">
+                  <span class="icon">
+                    <i class="fas fa-truck-loading" aria-hidden="true"></i>
+                  </span>
+                </button>
+                <button class="button is-danger is-small" @click="deleteComputer(stored_CI,i)">
+                  <span class="icon">
+                    <i class="fas fa-trash" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="column">
+        <h1 class="title has-background-danger">Retired Computers</h1>
+        <table class="table is-striped is-hoverable">
+          <thead>
+            <th>Service Tag</th>
+            <th>Location</th>
+            <th>Controls</th>
+          </thead>
+          <tbody>
+            <tr v-for="(retiredmachine, i) in retired_CI" :key="retiredmachine.Service_Tag">
+              <th>{{retiredmachine.Service_Tag}}</th>
+              <td>{{retiredmachine.Location}}</td>
+              <td>
+                <button class="button is-info is-small">
+                  <span class="icon">
+                    <i class="fas fa-bars" aria-hidden="true"></i>
+                  </span>
+                </button>
+                <button class="button is-warning is-small">
+                  <span class="icon">
+                    <i class="fas fa-truck-loading" aria-hidden="true"></i>
+                  </span>
+                </button>
+                <button class="button is-danger is-small" @click="deleteComputer(retired_CI,i)">
+                  <span class="icon">
+                    <i class="fas fa-trash" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </section>
 </template>
@@ -67,18 +115,26 @@ const stored_CI = Computer_Inventory.Inactive;
 const retired_CI = Computer_Inventory.Retired;
 
 export default {
-    data: () => ({
-        Computer_Inventory,
-        active_CI,
-        stored_CI,
-        retired_CI
-    }),
-    components: {
-        controlIcons 
+  data: () => ({
+    Computer_Inventory,
+    active_CI,
+    stored_CI,
+    retired_CI
+  }),
+  components: {
+    controlIcons
+  },
+  methods: {
+    viewInfo(i) {
+      console.log(this.Computer_Inventory.Active);
+    },
+    moveComputer(fromLoc, toLoc) {},
+    deleteComputer(deletelist, i) {
+      deletelist.splice(i, 1);
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
