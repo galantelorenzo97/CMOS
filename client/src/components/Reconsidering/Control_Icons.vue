5a14379667df2eed14b325c62bc22a189e5728a1
Lorenzo Galante <galantelorenzo97@gmail.com>
2020-04-27 13:18:18 -0400

    Moved All Components to Separate Folder. Redesign in progress

--- client/src/components/Reconsidering/Control_Icons.vue
+++ client/src/components/Reconsidering/Control_Icons.vue
@@ -1,34 +1,52 @@
 <template>
   <section class="container">
+
         <button class="button is-info is-small" @click="infoButtonPressed=true">
+
             <span class="icon">
                 <i class="fas fa-bars" aria-hidden="true"></i>
             </span>
+
         </button>
+        
         <button class="button is-warning is-small" @click="moveButtonPressed=true">
+
             <span class="icon">
                 <i class="fas fa-truck-loading" aria-hidden="true"></i>
             </span>
+
         </button>
+
         <button class="button is-danger is-small" @click="deleteButtonPressed=true">
+
             <span class="icon">
                 <i class="fas fa-trash" aria-hidden="true"></i>
             </span>
+        
         </button>
+
   </section>
 </template>
 
 <script>
-export let deleteButtonPressed = false;
-export let infoButtonPressed = false;
-export let moveButtonPressed = false;
+//export let deleteButtonPressed = false;
+//export let infoButtonPressed = false;
+//export let moveButtonPressed = false;
 
 export default {
+    //https://vuejs.org/v2/guide/components.html#data-Must-Be-a-Function
     data: () => ({
-      deleteButtonPressed,
-      infoButtonPressed,
-      moveButtonPressed  
-    })
+      
+    }),
+    props: {
+      deleteButtonPressed: Boolean
+    },
+    events: {
+
+    },
+    methods: {
+        
+    }
 }
 </script>
 
