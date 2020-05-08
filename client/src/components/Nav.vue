ed09617e10e4e71069ef57c0cf453710edb61957
Lorenzo Galante <galantelorenzo97@gmail.com>
2020-05-07 21:29:32 -0400

    Added Logo

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+      <a class="navbar-item" href="#">
+        <img src="http://cs.newpaltz.edu/~galantel1/CMOS_LOGO.PNG" width="112" height="28" />
       </a>
 
       <a
